485e538d7f09203503d1cecad8775f33c54a234e
verhovensky <105872686+verhovenskyivan@users.noreply.github.com>
2022-12-06 22:24:52 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Linq;
+namespace Solution
+{
+    public static class Program
+    {
+        public static string RepeatStr(int n, string s)
+        {
+            return String.Concat(Enumerable.Repeat(s, n));
+        }
+    }
+}
